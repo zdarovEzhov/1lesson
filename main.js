aeb5acaea68d7bab5e934ab514af9a962cc87dc9
zdarovEzhov <ezhov.95@gmail.com>
2022-10-10 20:27:18 +0300

    test commit

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Hello");
+let message = "Hello!";
+let userName = "John";
+let admin;
+admin = userName;
+console.log(admin);
+
+let str = "Привет";
+let str2 = "Одинарные кавычки тоже подойдут";
+let phrase = `Обратные кавычки позволяют встраивать переменные ${str}`;
+
+let яблоко = 1;
+let груша = 2;
+
+typeof яблоко;
+console.log(typeof яблоко);
+console.log(груша + яблоко);
